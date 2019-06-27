531b689975cfd6323420f4e6ef7e9637fd53bfb8
Srdjan Jovic <sjovic@domain.local>
2019-06-27 16:45:17 +0200

    Add more files to project

--- _protected/config/web.php
+++ _protected/config/web.php
@@ -11,7 +11,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => '7OQAFfUMIOfQx_8DFqcBdEjinuTWV-yA',
         ],
         // you can set your theme here - template comes with: 'light' and 'dark'
         'view' => [
