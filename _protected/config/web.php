3f02ef7cf98596a24f560525c5991f045f2c12b4
Srdjan Jovic <sjovic@domain.local>
2019-06-27 16:29:49 +0200

    Add new project files

---
+++ _protected/config/web.php
@@ -0,0 +1,100 @@
+<?php
+
+$params = require(__DIR__ . '/params.php');
+
+$config = [
+    'id' => 'basic',
+    'name' => 'BASIC',
+    'language' => 'en',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log', 'app\components\Aliases'],
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => '',
+        ],
+        // you can set your theme here - template comes with: 'light' and 'dark'
+        'view' => [
+            'theme' => [
+                'pathMap' => ['@app/views' => '@webroot/themes/light/views'],
+                'baseUrl' => '@web/themes/light',
+            ],
+        ],
+        'assetManager' => [
+            'bundles' => [
+                // we will use bootstrap css from our theme
+                'yii\bootstrap\BootstrapAsset' => [
+                    'css' => [], // do not use yii default one
+                ],
+            ],
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'urlManager' => [
+            'class' => 'yii\web\UrlManager',
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+                '<alias:\w+>' => 'site/<alias>',
+            ],
+        ],
+        'user' => [
+            'identityClass' => 'app\models\UserIdentity',
+            'enableAutoLogin' => true,
+        ],
+        'session' => [
+            'class' => 'yii\web\Session',
+            'savePath' => '@app/runtime/session'
+        ],
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+            'cache' => 'cache',
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            // send all mails to a file by default. 
+            // You have to set 'useFileTransport' to false and configure a transport for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'i18n' => [
+            'translations' => [
+                'app*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@app/translations',
+                    'sourceLanguage' => 'en',
+                ],
+                'yii' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@app/translations',
+                    'sourceLanguage' => 'en'
+                ],
+            ],
+        ],
+        'db' => require(__DIR__ . '/db.php'),
+    ],
+    'params' => $params,
+];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = ['class' => 'yii\debug\Module'];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = ['class' => 'yii\gii\Module'];
+}
+
+return $config;
