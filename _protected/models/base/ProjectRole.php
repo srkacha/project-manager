10a0a7429cc0cf8eac2f9f6b30fbb9e045e9917c
Srdjan Jovic <sjovic@domain.local>
2019-07-01 09:06:45 +0200

    Update model without blamable behaviour

--- _protected/models/base/ProjectRole.php
+++ _protected/models/base/ProjectRole.php
@@ -3,9 +3,6 @@
 namespace app\models\base;
 
 use Yii;
-use yii\behaviors\TimestampBehavior;
-use yii\behaviors\BlameableBehavior;
-use mootensai\behaviors\UUIDBehavior;
 
 /**
  * This is the base model class for table "project_role".
@@ -19,20 +16,6 @@ class ProjectRole extends \yii\db\ActiveRecord
 {
     use \mootensai\relation\RelationTrait;
 
-    private $_rt_softdelete;
-    private $_rt_softrestore;
-
-    public function __construct(){
-        parent::__construct();
-        $this->_rt_softdelete = [
-            'deleted_by' => \Yii::$app->user->id,
-            'deleted_at' => date('Y-m-d H:i:s'),
-        ];
-        $this->_rt_softrestore = [
-            'deleted_by' => 0,
-            'deleted_at' => date('Y-m-d H:i:s'),
-        ];
-    }
 
     /**
     * This function helps \mootensai\relation\RelationTrait runs faster
@@ -96,52 +79,6 @@ class ProjectRole extends \yii\db\ActiveRecord
         return $this->hasMany(\app\models\Participant::className(), ['project_role_id' => 'id', 'project_role_name' => 'name']);
     }
     
-    /**
-     * @inheritdoc
-     * @return array mixed
-     */
-    public function behaviors()
-    {
-        return [
-            'timestamp' => [
-                'class' => TimestampBehavior::className(),
-                'createdAtAttribute' => 'created_at',
-                'updatedAtAttribute' => 'updated_at',
-                'value' => new \yii\db\Expression('NOW()'),
-            ],
-            'blameable' => [
-                'class' => BlameableBehavior::className(),
-                'createdByAttribute' => 'created_by',
-                'updatedByAttribute' => 'updated_by',
-            ],
-            'uuid' => [
-                'class' => UUIDBehavior::className(),
-                'column' => 'id',
-            ],
-        ];
-    }
-
-    /**
-     * The following code shows how to apply a default condition for all queries:
-     *
-     * ```php
-     * class Customer extends ActiveRecord
-     * {
-     *     public static function find()
-     *     {
-     *         return parent::find()->where(['deleted' => false]);
-     *     }
-     * }
-     *
-     * // Use andWhere()/orWhere() to apply the default condition
-     * // SELECT FROM customer WHERE `deleted`=:deleted AND age>30
-     * $customers = Customer::find()->andWhere('age>30')->all();
-     *
-     * // Use where() to ignore the default condition
-     * // SELECT FROM customer WHERE age>30
-     * $customers = Customer::find()->where('age>30')->all();
-     * ```
-     */
 
     /**
      * @inheritdoc
@@ -149,7 +86,6 @@ class ProjectRole extends \yii\db\ActiveRecord
      */
     public static function find()
     {
-        $query = new \app\models\ProjectRoleQuery(get_called_class());
-        return $query->where(['project_role.deleted_by' => 0]);
+        return new \app\models\ProjectRoleQuery(get_called_class());
     }
 }
