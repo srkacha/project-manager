3949ae4e416a050ce8cb19e15318dba68c7fc296
Srdjan Jovic <sjovic@domain.local>
2019-07-02 18:02:08 +0200

    Save work

--- _protected/controllers/TaskController.php
+++ _protected/controllers/TaskController.php
@@ -40,4 +40,32 @@ class TaskController extends Controller
             'dataProvider' => $dataProvider,
         ]);
     }
+
+    /**
+     * @param integer @id
+     * @return mixed
+     */
+    public function actionView($id){
+        $model = $this->findModel($id);
+        
+        return $this->render('view', [
+            'model' => $this->findModel($id)
+        ]);
+    }
+
+    /**
+     * Finds the Supervisor model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return Task the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Task::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
 }
