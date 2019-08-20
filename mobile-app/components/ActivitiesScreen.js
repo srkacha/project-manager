c812430cc4f08cd2c91dcf4f690c02cfd2e88a29
Srdjan Jovic <sjovic51@gmail.com>
2019-08-20 20:22:58 +0200

    Add activities and progress component templates

--- mobile-app/components/ActivitiesScreen.js
+++ mobile-app/components/ActivitiesScreen.js
@@ -2,5 +2,37 @@ import React from 'react';
 import { StyleSheet, Text, View, Button, Image, TextInput, Alert } from 'react-native';
 
 export default class ActivitiesScreen extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+          user: this.props.navigation.getParam('user')
+        };
+      }
+  
+      static navigationOptions = ({ navigation }) =>{
+        return {
+          title: 'Projectory',
+          headerRight: (
+            <View style = {{paddingRight: 20}}><Button title="Logout" color="darkblue"
+              onPress={() => navigation.popToTop()}
+            /></View>),
+            headerLeft: null
+        }
+    };
+  
+    componentWillMount(){
+
+    }
+
+    componentDidMount(){
+    }
+
+    render(){
+    return (
+        <View>
+        <Text>Hello there</Text>
+        </View>
+    )
+    }
     
 }
