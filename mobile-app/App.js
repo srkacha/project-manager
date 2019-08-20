1c98b048d61fdf1d6e356bea2642b43d76f2824f
Srdjan Jovic <sjovic51@gmail.com>
2019-08-20 13:36:05 +0200

    Create mobile app login screen, implement navigation

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -1,19 +1,71 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, TextInput } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+//Code representing the screens that will be rendered
+
+class LoginScreen extends React.Component {
+  static navigationOptions = {
+  };
+  render() {
+    return (
+    <View style={{paddingTop:20, alignItems: 'center'}}>
+      <Text style = {styles.loginText}>Welcome to Projectory</Text>  
+      <Image style = {styles.loginImage} source = {require('./assets/project.png')}></Image>
+      <TextInput placeholder = 'Username' style = {styles.loginInput}></TextInput>
+      <TextInput placeholder = 'Password' secureTextEntry = {true} style = {styles.loginInput}></TextInput>
+      <Button color = 'darkblue' style = {styles.loginButton} title = 'Login'/>
     </View>
-  );
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+//Code responsible for navigation
+
+const MainNavigator = createStackNavigator(
+  {
+    Login: {screen: LoginScreen},
   },
+  {
+    initialRouteName: "Login",
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: 'white',
+      },
+      headerTintColor: 'black',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+    },
+  }
+);
+
+//Exporting the main app object
+
+const App = createAppContainer(MainNavigator);
+export default App;
+
+//Styles for the screens
+
+const styles = StyleSheet.create({
+  loginImage: {
+    height: 250,
+    width: 250, 
+    marginBottom: 25
+  },
+  loginInput: {
+    borderBottomColor: 'gray', 
+    borderBottomWidth: 1, 
+    marginBottom:25,
+    width: 250
+  },
+  loginText: {
+    fontSize:40,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    marginBottom:25
+  },
+  loginButton: {
+    width: 250
+  }
 });
